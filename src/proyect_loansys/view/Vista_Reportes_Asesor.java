5264068632a8057828bd2c4a11083cd964238e4f
stevanotero <juanstebanotero608@gmail.com>
2026-07-22 17:53:57 -0500

    se elimino en reportes el boton limpiar

--- src/proyect_loansys/view/Vista_Reportes_Asesor.java
+++ src/proyect_loansys/view/Vista_Reportes_Asesor.java
@@ -24,7 +24,7 @@ import javax.swing.table.DefaultTableModel;
  */
 public class Vista_Reportes_Asesor extends Vista_Principal {
 
-    public JButton btnEstadoGeneral, btnFrecuenciaUso, btnAlertasMantenimiento, btnExportarPDF, btnLimpiar;
+    public JButton btnEstadoGeneral, btnFrecuenciaUso, btnAlertasMantenimiento, btnExportarPDF;
     public JTable tablaReportes;
     public DefaultTableModel modelo;
 
@@ -45,20 +45,20 @@ public class Vista_Reportes_Asesor extends Vista_Principal {
         btnFrecuenciaUso = new JButton("Frecuencia de Uso");
         btnAlertasMantenimiento = new JButton("Alertas de Mantenimiento");
         btnExportarPDF = new JButton("Exportar a PDF");
-        btnLimpiar = new JButton("Limpiar Tabla");
+
 
         // Colores
         btnEstadoGeneral.setBackground(new Color(0, 153, 76));
         btnFrecuenciaUso.setBackground(new Color(0, 102, 204));
         btnAlertasMantenimiento.setBackground(new Color(204, 102, 0));
         btnExportarPDF.setBackground(new Color(139, 0, 0));
-        btnLimpiar.setBackground(new Color(128, 128, 128));
+    
 
         btnEstadoGeneral.setForeground(Color.WHITE);
         btnFrecuenciaUso.setForeground(Color.WHITE);
         btnAlertasMantenimiento.setForeground(Color.WHITE);
         btnExportarPDF.setForeground(Color.WHITE);
-        btnLimpiar.setForeground(Color.WHITE);
+
 
         // Panel botones
         JPanel panelBotones = new JPanel(new FlowLayout(FlowLayout.LEFT, 12, 12));
@@ -69,7 +69,7 @@ public class Vista_Reportes_Asesor extends Vista_Principal {
         panelBotones.add(btnFrecuenciaUso);
         panelBotones.add(btnAlertasMantenimiento);
         panelBotones.add(btnExportarPDF);
-        panelBotones.add(btnLimpiar);
+    
 
         // Tabla
         modelo = new DefaultTableModel();
