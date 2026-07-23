3b0688b53d03a269cd44b5082df4ca53d83ddc96
zDanielcv <ceballosvalenzueladaniel@gmail.com>
2026-07-22 22:03:21 -0500

    modulo tecnico terminado de Daniel y santiago

--- src/proyect_loansys/view/GenerarReporte_Tecnico.java
+++ src/proyect_loansys/view/GenerarReporte_Tecnico.java
@@ -9,6 +9,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.UIManager; // <-- Importante para los JComboBox
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class GenerarReporte_Tecnico extends Plantilla {
 
